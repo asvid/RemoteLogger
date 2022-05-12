1781211fd4da984cb86d0076c7f7dd42bfc652ac
Adam Świderski <adam.swiderski89@gmail.com>
2022-05-12 14:57:53 +0200

    project update

--- ExampleApp/src/main/java/com/github/asvid/remoteloggerapp/App.kt
+++ ExampleApp/src/main/java/com/github/asvid/remoteloggerapp/App.kt
@@ -9,7 +9,6 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Log.d("APP", "onCreate")
         RemoteLogger().initialize(
             Config(
                 "192.168.1.87",
